bdc7dcee1633bccb5f39d8503200b8ba94f477ac
chouser <tranquochung567@gmail.com>
2026-03-18 18:10:39 +0700

    feat(ui): implement root layout and global styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
-import Image from "next/image";
+import Header from "@/components/header";
+import Featured from "@/components/Feature";
+import clientPromise from "@/lib/mongodb";
+import { ObjectId } from "mongodb";
+import NewProduct from "@/components/NewProduct";
+export default async function HomePage() {
+  const featuredProductId = "69b84732a4d485e6a740e9be";
 
-export default function Home() {
+  const client = await clientPromise;
+  const db = client.db();
+
+  const featureProduct = await db
+    .collection("products")
+    .findOne({ _id: new ObjectId(featuredProductId) });
+    const newProducts = await db
+    .collection("products")
+    .find({})
+    .sort({ _id: -1 }) // mới nhất
+    .limit(10)
+    .toArray();
   return (
-    <div>Init project</div>
+    <div>
+      <Header />
+      <Featured product={JSON.parse(JSON.stringify(featureProduct))} />
+      <NewProduct products={JSON.parse(JSON.stringify(newProducts))}/>
+    </div>
   );
 }
